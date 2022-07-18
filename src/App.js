92f2264ea27cd74f039096ac6398f69e5e24c67a
fuadafif <fuadafif95@gmail.com>
2022-07-18 17:00:53 +0800

    SPA

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-// import { Button } from "reactstrap";
+import { Button } from "reactstrap";
 
-// function App() {
-//   let tampilkanPesan = (e) => {
-//     alert("Anda Logout");
-//   };
-//   return (
-//     <div>
-//       <button onClick={tampilkanPesan}>Logout</button>
-//       <Button onClick={tampilkanPesan}>Logout</Button>
-//     </div>
-//   );
-// }
-// export default App;
+function App() {
+  let tampilkanPesan = (e) => {
+    alert("Anda Logout");
+  };
+  return (
+    <div>
+      <Button onClick={tampilkanPesan}>Logout</Button>
+    </div>
+  );
+}
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import HeaderNav from "./components/navbar/HeaderNav";
+import LandingPage from "./pages/LandingPage";
+import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import GameList from "./pages/GameList";
+import PageNotFound from "./pages/PageNotFound";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -11,10 +16,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
+    <HeaderNav />
     <Routes>
-      <Route path="/" element={<App />} />
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/home" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
+      <Route path="/gameList" element={<GameList />} />
+      <Route path="*" element={<PageNotFound />} />
     </Routes>
   </BrowserRouter>
 );
