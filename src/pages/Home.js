7935e92ad4db409eedb79f4d8dd32f1e8b84dc46
Bian Damara <bian.damara@gmail.com>
2022-07-20 18:04:01 +0700

    Edit Home Page with Bootstrap

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,31 @@
+// import { Img } from "reactstrap";
+
 import LandingNav from "../components/navbar/HomeNav";
+
+import "./home-assets/css/Home.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function Home() {
   return (
-    <>
-      < LandingNav />
-      <h1>HOME PAGE</h1>
-      <a href="/play-game">Rock Paper Scissor</a>
-    </>
+    <div>
+      <LandingNav />
+      <img src="./home-assets/img/player.png" alt="profile picture"></img>
+      <h5>Username</h5>
+
+      <hr />
+
+      <div className="col">
+        <div className="card">
+          <a href="/play-game">
+            <img src="./home-assets/img/rock-paper-scissors.png" class="card-img-top" alt="Rock Paper Scissor" />
+          </a>
+          <div className="card-body">
+            <h5 className="card-title">Rock Paper Scissor</h5>
+            <p className="card-text">Rock-Paper-Scissors is a two-person hand game. This game is often used for random selection. The players choose one {`(eg, "Rock! Paper! Scissors!")`}. </p>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
