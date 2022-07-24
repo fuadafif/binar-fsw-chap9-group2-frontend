3d23d8a4115aad1c170c68808298b845fbad1be2
Bian Damara <bian.damara@gmail.com>
2022-07-25 00:28:23 +0700

    Edir Home Page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,28 +1,32 @@
-import HomeNav from "../components/navbar/HomeNav"
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+import HomeNav from "../components/navbar/HomeNav";
 import Footer from "../components/Footer/Footer";
+
 import "../assets/home/css/Home.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+
+import PicProfile from "../assets/home/img/pic-profile.png";
 import RPSGame from "../assets/home/img/rock-paper-scissors.jpg";
 import SnakeGame from "../assets/home/img/snake-game.jpg";
-import { useNavigate } from 'react-router-dom';
-import { useEffect } from 'react';
-import PicProfile from '../assets/home/img/pic-profile.png';
+import Chess from "../assets/home/img/chess.webp";
+import Solitaire from "../assets/home/img/solitaire.jpeg";
+import CTR from "../assets/home/img/ctr.jpeg";
 
 function Home() {
-
   const navigate = useNavigate();
 
   useEffect(() => {
     const emailKey = localStorage.getItem("email");
-    const name = document.getElementById('name');
+    const name = document.getElementById("name");
     const logoutButton = document.getElementById("logout");
-    if(emailKey) {
+    if (emailKey) {
       name.innerHTML = `Hi!! ${emailKey}`;
       logoutButton.hidden = false;
     } else {
       name.innerHTML = `Welcome`;
       logoutButton.hidden = true;
-
     }
   });
 
@@ -33,19 +37,20 @@ function Home() {
     localStorage.removeItem(removeEmail);
     localStorage.removeItem(removeAuth);
     window.location.reload();
-    navigate('/home');
+    navigate("/home");
   }
 
   return (
-    <div>
+    <div className="pg-css">
       <HomeNav />
       <div className="text-center">
         <img src={PicProfile} alt="profile pic" className="profile"></img>
         <br />
         <h2 id="name"></h2>
-        <button onClick={logout} className="btn btn-danger" id="logout" hidden>Logout</button>
+        <button onClick={logout} className="btn btn-danger" id="logout" hidden>
+          Logout
+        </button>
       </div>
-
 
       <hr />
 
@@ -59,7 +64,11 @@ function Home() {
             <img src={RPSGame} className="card-img-top img-game" alt="rock paper scissor" />
             <div className="card-body">
               <h5 className="card-title">Rock Paper Scissor</h5>
-              <p className="card-text">Rock-Paper-Scissors is a two-person hand game. This game is often used for random selection. The players choose one {`(eg, "Rock! Paper! Scissors!")`}. </p>
+              <p className="card-text">
+                Rock-Paper-Scissors is a two-person hand game. This game is often used for random selection. The players choose one {`(eg, "Rock! Paper! Scissors!")`}.
+                <br />
+                <br />
+              </p>
               <a href="/play-game" class="btn btn-primary">
                 Play Now
               </a>
@@ -73,7 +82,61 @@ function Home() {
             <div className="card-body">
               <h5 className="card-title">Snake</h5>
               <p className="card-text">
-                The player controls a long, thin creature, resembling a snake, which roams around on a bordered plane, picking up food (or some other item), trying to avoid hitting its own tail or the edges of the playing area.{" "}
+                The player controls a long, thin creature, resembling a snake, which roams around on a bordered plane, picking up food, trying to avoid hitting its own tail or the edges of the playing area.
+                <br />
+              </p>
+              <a href="/home" class="btn btn-primary">
+                Play Now
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className="col game">
+          <div className="card">
+            <img src={Chess} className="card-img-top img-game" alt="snake" />
+            <div className="card-body">
+              <h5 className="card-title">Chess</h5>
+              <p className="card-text">
+                Chess is a board game played between two players.
+                <br />
+                <br />
+                <br />
+                <br />
+              </p>
+              <a href="/home" class="btn btn-primary">
+                Play Now
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className="col game">
+          <div className="card">
+            <img src={Solitaire} className="card-img-top img-game" alt="snake" />
+            <div className="card-body">
+              <h5 className="card-title">Solitaire</h5>
+              <p className="card-text">
+                Patience, card solitaire or solitaire, is a genre of card games whose common feature is that the aim is to arrange the cards in some systematic order.
+                <br />
+                <br />
+              </p>
+              <a href="/home" class="btn btn-primary">
+                Play Now
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className="col game">
+          <div className="card">
+            <img src={CTR} className="card-img-top img-game" alt="snake" />
+            <div className="card-body">
+              <h5 className="card-title">Crash Team Racing</h5>
+              <p className="card-text">
+                The game's story focuses on the efforts of Crash Bandicoot, Doctor Neo Cortex, who must race against the egomaniacal Nitros Oxide to save the Earth from destruction.
+                <br />
+                <br />
               </p>
               <a href="/home" class="btn btn-primary">
                 Play Now
